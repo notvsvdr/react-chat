70d383ae323851df49365c85a0f8a2354a0f6be9
vs <not.saavedra@gmail.com>
2019-01-31 01:53:55 +0200

    Updated readme file. Created boilerplate for chat panels.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import './App.css';
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>App</div>
-        );
-    }
+const App = () => {
+    return (
+        <Grid columns='equal' className='app' style={{ background: '#eee' }}>
+            <ColorPanel />
+            <SidePanel />
+            <Grid.Column style={{ marginLeft: 220 }}>
+                <Messages />
+            </Grid.Column>
+            <Grid.Column width={4}>
+                <MetaPanel />
+            </Grid.Column>
+
+        </Grid>
+    );
 }
 
 export default App;
---
+++ src/components/MetaPanel/MetaPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MetaPanel extends Component {
+    render() {
+        return (
+            <div>
+                SidePanel
+            </div>
+        );
+    }
+}
+
+export default MetaPanel;
