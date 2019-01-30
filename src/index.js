2969056c5220c67bf7934c6f5ff8af476cf13a89
vs <not.saavedra@gmail.com>
2019-01-31 01:18:06 +0200

    Set up redux. Eject create-react-app. Added spinner.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router
 import App from './components/App';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
+import Spinner from './components/spinner';
 
 // style
 import 'semantic-ui-css/semantic.min.css';
@@ -19,35 +20,44 @@ import firebase from './firebase';
 
 // redux
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider, connect } from 'react-redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions/index';
 
-const store = createStore(() => {
-
-}, composeWithDevTools())
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class Root extends React.Component {
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
+                this.props.setUser(user);
                 this.props.history.push('/');
             }
-        })
+        });
     }
 
     render() {
         return (
-            <Switch>
-                <Route exact path='/' component={App} />
-                <Route path='/register' component={Register} />
-                <Route path='/login' component={Login} />
-            </Switch>
+            this.props.isLoading ?
+                <Spinner /> :
+                <Switch>
+                    <Route exact path='/' component={App} />
+                    <Route path='/register' component={Register} />
+                    <Route path='/login' component={Login} />
+                </Switch>
         );
     }
 }
 
-const RootWithRoute = withRouter(Root);
+const mapStateToProps = (state) => {
+    return {
+        isLoading: state.user.isLoading
+    }
+}
+
+const RootWithRoute = withRouter(connect(mapStateToProps, { setUser })(Root));
 
 ReactDOM.render(
     <Provider store={store}>
